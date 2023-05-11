878ec981a9fbc2914207bd4c02a1f158168715f0
Alan Tavares <alan1tavares@gmail.com>
2023-05-10 23:22:55 -0300

    Alterado de MUI para chackra ui
    Achei a integração mais simplse de fazer com o nextjs

---
+++ src/app/theme.ts
@@ -0,0 +1,43 @@
+// theme.ts (tsx file with usage of StyleFunctions, see 4.)
+import { extendTheme } from '@chakra-ui/react'
+import type { StyleFunctionProps } from '@chakra-ui/styled-system'
+
+const theme = extendTheme({
+    colors: {
+        brand: {
+          100: "#f7fafc",
+          // ...
+          900: "#1a202c",
+        },
+        primary: {
+          500: '#FFA4A3',
+          600: '#D18686',
+          700: '#A36968',
+          900: '#472E2E'  
+        },
+      },
+    components: {
+        Button: {
+            variants: {
+                solid: {
+                    color: 'primary.900',
+                    borderRadius: '16px',
+                    boxShadow: '0px 4px 4px rgba(64, 41, 41, 0.25)',
+                }
+            },
+
+            sizes: {
+                md: {
+                    px: 6,
+                }
+            },
+            
+            defaultProps: {
+                colorScheme: 'primary',
+                sizes: 'md',
+            },
+        },
+    },
+})
+
+export default theme
